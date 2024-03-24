f5b35eea327770b56c766be15fec7e8e205abf7e
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-03-24 14:54:09 +0000

    feat: add user and auth services

--- package.sh
+++ package.sh
@@ -10,3 +10,22 @@ npx prisma init
 ```bash
 npx prisma migrate dev
 ```
+
+## devDependencies
+```bash
+    yarn add -D nodemon
+    yarn add -D ts-node
+    yarn add -D typescript
+    yarn add -D @types/node
+    yarn add -D @prisma/client
+    yarn add -D @types/express
+    yarn add -D @types/morgan
+    yarn add -D @types/cors
+    yarn add -D @types/mongoose 
+    yarn add -D @types/axios
+    yarn add -D @types/bcrypt
+    yarn add -D @types/jsonwebtoken
+    yarn add -D @types/uuid
+    yarn add -D tsc tsc-alias tsconfig-paths
+``
+## oo   
