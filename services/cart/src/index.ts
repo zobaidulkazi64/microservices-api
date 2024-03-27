fe1398981bf87165be64d7b4ac1eea4405a4f61b
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-03-27 23:18:49 +0600

    add cart services

---
+++ services/cart/src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+import router from "./routes";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/email", router);
+
+app.get("/health", (req, res) => {
+  res.send(`Hello!, I am live on!, on port 4004`);
+});
+
+const port = process.env.PORT || 40045;
+const serviceName = process.env.SERVICE_NAME || "email-service";
+
+app.listen(port, () => {
+  console.log(`${serviceName} started on port ${port}`);
+});
