3f2eeba5d4d1459ace3e69f0dc7f3f699b4acdde
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-03-31 14:46:01 +0600

    zk: add config types

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,11 @@
 const types = {
-    "axios": "axios",
-    "ts-node": "ts-node",
-    "typescript": "typescript",
-    "prisma": "prisma",
-    "cors": "cors",
-    "morgan": "morgan",
-}
+  axios: "axios",
+  "ts-node": "ts-node",
+  typescript: "typescript",
+  prisma: "prisma",
+  cors: "cors",
+  morgan: "morgan",
+  dotenv: "dotenv",
+  express: "express",
+  "ts-node-dev": "ts-node-dev",
+};
