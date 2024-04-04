379f068788150b63a898fa0b65de18f65de4eae7
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-04 21:30:31 +0600

    get if product not valid

--- services/product/src/controllers/getProductById.ts
+++ services/product/src/controllers/getProductById.ts
@@ -5,22 +5,25 @@ const prisma = new PrismaClient();
 const router = express.Router();
 
 // Get a product by ID
-router.get("/products/:id", async (req: Request, res: Response) => {
-  const productId = parseInt(req.params.id);
-  try {
-    const product = await prisma.product.findUnique({
-      where: {
-        id: productId,
-      },
-    });
-    if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+const getProductById = router.get(
+  "/products/:id",
+  async (req: Request, res: Response) => {
+    const productId = parseInt(req.params.id);
+    try {
+      const product = await prisma.product.findUnique({
+        where: {
+          id: productId,
+        },
+      });
+      if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+      }
+      res.json(product);
+    } catch (error) {
+      console.error("Error retrieving product:", error);
+      res.status(500).json({ error: "Internal server error" });
     }
-    res.json(product);
-  } catch (error) {
-    console.error("Error retrieving product:", error);
-    res.status(500).json({ error: "Internal server error" });
   }
-});
+);
 
-export default router;
+export default getProductById;
