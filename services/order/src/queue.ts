22317fbaa1bddbedd0d167913e8951fda1992c71
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-05 21:44:40 +0600

    queue add by order

--- services/order/src/queue.ts
+++ services/order/src/queue.ts
@@ -2,4 +2,21 @@ import amqp from "amqplib";
 
 const sendToQueue = async (queue: string, message: string) => {
   // create connection
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const exchangeName = "order";
+  await channel.assertExchange(exchangeName, "direct", {
+    durable: true,
+  });
+
+  channel.publish(exchangeName, queue, Buffer.from(message));
+
+  console.log(`Message sent to queue ${queue}`);
+
+  setTimeout(() => {
+    connection.close();
+  }, 500);
 };
+
+export default sendToQueue;
