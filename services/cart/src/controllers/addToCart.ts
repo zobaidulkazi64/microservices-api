113499ed21153c0ed844fabc5cdba0e252cddfe0
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-04-06 22:09:30 +0600

    zk: controller start

--- services/cart/src/controllers/addToCart.ts
+++ services/cart/src/controllers/addToCart.ts
@@ -2,3 +2,25 @@ import { Request, Response, NextFunction } from "express";
 import axios from "axios";
 import { v4 as uuid } from "uuid";
 import { CART_TTL, INVENTORY_SERVICE } from "../../config";
+import { CartItemSchema } from "../../schemas";
+
+const addToCart = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    // validate request body
+    const parsedBody = CartItemSchema.safeParse(req.body);
+    if (!parsedBody.success) {
+      return res.status(400).json({
+        success: false,
+        message: parsedBody.error.message,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export default addToCart;
